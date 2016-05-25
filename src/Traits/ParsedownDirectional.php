ea1251c39aac3b729ccd010d6924403da261e685
Joshua Moon <josh@jaw.sh>
2016-05-25 02:10:06 -0500

    Added helper functions for accessing rtl/ltr.

--- src/Traits/ParsedownDirectional.php
+++ src/Traits/ParsedownDirectional.php
@@ -4,10 +4,59 @@ namespace InfinityNext\Eightdown\Traits;
 
 trait ParsedownDirectional
 {
-    public $RtlRegex = "\u{0600}-\u{06ff}|\u{0750}-\u{077f}|\u{fb50}-\u{fc3f}|\u{fe70}-\u{fefc}";
-    public $textLtr = 0;
-    public $textRtl = 0;
-    public $textDir = "ltr";
+    /**
+     * Hebrew and Arabic characters.
+     *
+     * @var string
+     */
+    protected $RtlRegex = "\u{0600}-\u{06ff}|\u{0750}-\u{077f}|\u{fb50}-\u{fc3f}|\u{fe70}-\u{fefc}";
+
+    /**
+     * Tally of left-to-right characters.
+     *
+     * Does not include punctuation or currency symbols, only human language.
+     *
+     * @var integer
+     */
+    protected $textLtr = 0;
+
+    /**
+     * Hebrew and Arabic characters.
+     *
+     * @var integer
+     */
+    protected $textRtl = 0;
+
+    /**
+     * The determined post direction.
+     *
+     * In instances with inadequate or obtuse characters (like ">>5555"), it
+     * will remain null and set to neither left nor right. In this case, the
+     * post should not set a direction and follow the flow of the page.
+     *
+     * @var string|null
+     */
+    protected $textDir = null;
+
+    /**
+     * Is this parser's last processed text RTL?
+     *
+     * @return boolean true
+     */
+    public function isRtl()
+    {
+        return $this->textDir === "rtl";
+    }
+
+    /**
+     * Is this parser's last processed text LTR?
+     *
+     * @return boolean true
+     */
+    public function isLtr()
+    {
+        return $this->textDir === "ltr";
+    }
 
     public function text($text)
     {
@@ -17,6 +66,10 @@ trait ParsedownDirectional
         if ($this->textRtl > 0 && $this->textRtl > $this->textLtr)
         {
             $this->textDir = "rtl";
+        }
+        else if ($this->textLtr > 0 && $this->textLtr > $this->textRtl)
+        {
+            $this->textDir = "ltr";
         }
 
         return parent::text($text);
@@ -53,11 +106,11 @@ trait ParsedownDirectional
             $dir = null;
 
 
-            if ($this->textDir == "ltr" && $rtl > 0 && $rtl > $ltr)
+            if ($this->isLtr() && $rtl > 0 && $rtl > $ltr)
             {
                 $dir = "rtl";
             }
-            else if ($this->textDir == "rtl" && $ltr > 0 && $ltr > $rtl)
+            else if ($this->isRtl() && $ltr > 0 && $ltr > $rtl)
             {
                 $dir = "ltr";
             }
